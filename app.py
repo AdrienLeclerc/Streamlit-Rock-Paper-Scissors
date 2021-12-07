921be0d2ad4f9cbf0b70adf527081f9a4a167efa
AdrienLeclerc <91472128+AdrienLeclerc@users.noreply.github.com>
2021-12-07 16:21:44 +0100

    Update app.py

--- app.py
+++ app.py
@@ -48,7 +48,7 @@ def teachable_machine_classification(img, file):
 
 st.image("https://i.imgur.com/CikgsKT.png")
 st.title("Image Classification avec Teachable Machine de Google !")
-st.text("Plus d'information sur Teachable Machine par ici : https://teachablemachine.withgoogle.com/")
+st.text("Plus d'information sur Teachable Machine par ici : [link](https://teachablemachine.withgoogle.com)")
 st.header("Normal X Ray Vs Pneumonia X Ray")
 st.text("Upload a X Ray to detect it is normal or has pneumonia")
 # file upload and handling logic
